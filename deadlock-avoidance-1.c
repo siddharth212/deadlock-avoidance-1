b4753b3797746ee5bce87e207a02134506a52616
siddharth212 <49288164+siddharth212@users.noreply.github.com>
2019-04-17 08:11:50 +0530

    Update deadlock-avoidance-1.c

--- deadlock-avoidance-1.c
+++ deadlock-avoidance-1.c
@@ -34,3 +34,69 @@ int main()
     printf("\n\nEnter the Available Resources : ");
     for(i = 0; i < r; i++)
         scanf("%d", &avail[i]);
+    
+    for(i = 0; i < p; i++)
+
+        for(j = 0; j < r; j++)
+            need[i][j] = Max[i][j] - alloc[i][j];
+
+        do
+        {
+            printf("\n Max matrix:\tAllocation matrix:\n");
+
+            for(i = 0; i < p; i++)
+            {
+                for( j = 0; j < r; j++)
+                    printf("%d ", Max[i][j]);
+                printf("\t\t");
+                for( j = 0; j < r; j++)
+                    printf("%d ", alloc[i][j]);
+                printf("\n");
+            }
+
+            process = -1;
+
+            for(i = 0; i < p; i++)
+            {
+                if(completed[i] == 0)//if not completed
+                {
+                    process = i ;
+                    for(j = 0; j < r; j++)
+                    {
+                        if(avail[j] < need[i][j])
+                        {
+                            process = -1;
+                            break;
+                        }
+                    }
+                }
+                if(process != -1)
+                    break;
+            }
+
+            if(process != -1)
+            {
+                printf("\nProcess %d runs to completion!", process + 1);
+                safeSequence[count] = process + 1;
+                count++;
+                for(j = 0; j < r; j++)
+                {
+                    avail[j] += alloc[process][j];
+                    alloc[process][j] = 0;
+                    Max[process][j] = 0;
+                    completed[process] = 1;
+                }
+            }
+        }
+        while(count != p && process != -1);
+
+        if(count == p)
+        {
+            printf("\nThe system is in a safe state!!\n");
+            printf("Safe Sequence : < ");
+            for( i = 0; i < p; i++)
+                printf("%d ", safeSequence[i]);
+            printf(">\n");
+        }
+        else
+            printf("\nThe system is in an unsafe state!!");
